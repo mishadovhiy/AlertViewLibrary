f6e62d9fd187937d3bcd875dbc636312759de48e
Misha Dovhiy <hi@dovhiy.com>
2022-06-06 23:07:42 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# AlertViewLibrary
+
+A description of this package.
---
+++ Sources/AlertViewLibrary/AlertViewLibrary.swift
@@ -0,0 +1,6 @@
+public struct AlertViewLibrary {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
