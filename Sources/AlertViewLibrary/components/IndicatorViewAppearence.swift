20a503a7468630d3ff0744f0162e89f5068db74b
Misha Dovhiy <hi@dovhiy.com>
2022-06-08 04:39:57 +0300

    image fixes

--- Sources/AlertViewLibrary/components/IndicatorViewAppearence.swift
+++ Sources/AlertViewLibrary/components/IndicatorViewAppearence.swift
@@ -57,13 +57,13 @@ extension AlertViewLibrary {
         if let image = image {
             return .init(named: image.rawValue)
         } else {
-            let error:UIImage? = (type == .error || type == .internetError) ? image(named: Image.error.rawValue) : nil
-            let scs:UIImage? = (type == .succsess) ? image(named: Image.succsess.rawValue) : nil
+            let error:UIImage? = (type == .error || type == .internetError) ? imageNamed(Image.error.rawValue) : nil
+            let scs:UIImage? = (type == .succsess) ? imageNamed(Image.succsess.rawValue) : nil
             return error ?? (scs ?? nil)
         }
     }
     
-    private func image(named:String) -> UIImage? {
+    private func imageNamed(_ named:String) -> UIImage? {
         return .init(named: named, in: Bundle.module, compatibleWith: nil)
     }
     
