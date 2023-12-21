7c6c01800c516ad34b4a6cdb10acfba84a70837a
mishadovhiy <hi@mishadovhiy.com>
2023-12-21 15:05:31 +0200

    hide ai when alert

--- Sources/AlertViewLibrary/AlertViewLibrary.swift
+++ Sources/AlertViewLibrary/AlertViewLibrary.swift
@@ -51,7 +51,7 @@ public class AlertViewLibrary: UIView {
                 self.mainView.layer.transform = CATransform3DTranslate(CATransform3DIdentity, 0, 0, 0)
                 
             } completion: { (_) in
-         //       self.activityIndicatorView.superview?.layoutIfNeeded()
+                self.activityIndicatorView.superview?.layoutIfNeeded()
                 completion?()
             }
         }
